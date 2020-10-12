cfcdee8ffb1ce09b1d9acd02a77bba70adfc174e
NikitaTumanov <57438814+NikitaTumanov@users.noreply.github.com>
2020-10-12 20:19:50 +0300

    remake11

--- prictice/pract11-Tumanov/src/com/company/Main.java
+++ prictice/pract11-Tumanov/src/com/company/Main.java
@@ -5,32 +5,20 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
-        arrayQueue.add(123);
-        arrayQueue.add(434);
-        arrayQueue.add(524);
-        arrayQueue.add(489);
-        System.out.println("----------ArrayQueue----------");
-        System.out.println(arrayQueue);
-        System.out.println("First element: " + arrayQueue.element());
-        System.out.println(arrayQueue);
-        System.out.println("Poll: " + arrayQueue.poll());
-        System.out.println(arrayQueue);
-        System.out.println();
+        for(int i=0; i<6;i++) {
+            arrayQueue.add((int) (Math.random()*100));
+        }
+        System.out.println("ArrayQueue: "+ arrayQueue);
+        System.out.println("Poll: " + arrayQueue.poll() + "\t"+arrayQueue);
+        System.out.println("First element: " + arrayQueue.element() + "\t"+arrayQueue+"\n");
 
         LinkedQueue<Integer> linkedQueue = new LinkedQueue<>();
         ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(12, 43, 55, 67, 80));
         linkedQueue.addAll(arrayList);
-        linkedQueue.offer(1);
-        linkedQueue.offer(2);
-        linkedQueue.offer(3);
-        linkedQueue.offer(4);
-        linkedQueue.offer(5);
-        System.out.println("----------LinkedQueue----------");
-        System.out.println(linkedQueue);
+        linkedQueue.offer(45);
+        System.out.println("LinkedQueue: " + linkedQueue);
         System.out.println("Queue size: " + linkedQueue.size());
-        System.out.println("Peek: " + linkedQueue.peek());
-        System.out.println(linkedQueue);
-        System.out.println("Poll: " + linkedQueue.poll());
-        System.out.println(linkedQueue);
+        System.out.println("Poll: " + linkedQueue.poll() + "\t"+linkedQueue);
+        System.out.println("Peek: " + linkedQueue.peek() + "\t"+linkedQueue);
     }
 }
